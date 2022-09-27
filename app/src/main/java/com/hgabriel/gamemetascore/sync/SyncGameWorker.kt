5c2e2a3bd280b3361adc1418525fe830ac60c6f3
Hugo Gabriel <7658465+hgabriel84@users.noreply.github.com>
2022-09-27 15:49:32 +0100

    implement getForegroundInfo in CoroutineWorker
    this is needed to prevent an exception of being thrown in lower APIs

--- app/src/main/java/com/hgabriel/gamemetascore/sync/SyncGameWorker.kt
+++ app/src/main/java/com/hgabriel/gamemetascore/sync/SyncGameWorker.kt
@@ -1,15 +1,18 @@
 package com.hgabriel.gamemetascore.sync
 
 import android.content.Context
+import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingWorkPolicy
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import com.hgabriel.gamemetascore.R
 import com.hgabriel.gamemetascore.data.GameRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -18,7 +21,7 @@ import kotlinx.coroutines.withContext
 
 @HiltWorker
 class SyncGameWorker @AssistedInject constructor(
-    @Assisted context: Context,
+    @Assisted private val context: Context,
     @Assisted params: WorkerParameters,
     private val repository: GameRepository
 ) : CoroutineWorker(context, params) {
@@ -28,7 +31,21 @@ class SyncGameWorker @AssistedInject constructor(
         Result.success()
     }
 
+    override suspend fun getForegroundInfo(): ForegroundInfo =
+        ForegroundInfo(SyncNotificationId, getNotification())
+
+    private fun getNotification() =
+        NotificationCompat.Builder(context, SyncNotificationChannelID)
+            .setSmallIcon(R.drawable.ic_videogame_24)
+            .setContentTitle(context.getString(R.string.app_name))
+            .setContentText(context.getString(R.string.sync_games_background))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
     companion object {
+        private const val SyncNotificationId = 0
+        private const val SyncNotificationChannelID = "SyncNotificationChannel"
+
         fun sync(context: Context) {
             val syncConstraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
