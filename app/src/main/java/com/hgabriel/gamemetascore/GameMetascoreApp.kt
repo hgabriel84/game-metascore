1e88d8a2e5893db0157b098613e62c0e5486a563
Hugo Gabriel <7658465+hgabriel84@users.noreply.github.com>
2022-09-27 12:14:39 +0100

    move worker init code from application file

--- app/src/main/java/com/hgabriel/gamemetascore/GameMetascoreApp.kt
+++ app/src/main/java/com/hgabriel/gamemetascore/GameMetascoreApp.kt
@@ -4,11 +4,6 @@ import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.viewbinding.BuildConfig
 import androidx.work.Configuration
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.OutOfQuotaPolicy
-import androidx.work.WorkManager
 import com.hgabriel.gamemetascore.sync.SyncGameWorker
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -25,22 +20,9 @@ class GameMetascoreApp : Application(), Configuration.Provider {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-        syncGame()
+        SyncGameWorker.sync(applicationContext)
     }
 
     override fun getWorkManagerConfiguration() =
         Configuration.Builder().setWorkerFactory(workerFactory).build()
-
-    private val syncConstraints
-        get() = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-
-    private fun syncGame() {
-        val request = OneTimeWorkRequestBuilder<SyncGameWorker>()
-            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
-            .setConstraints(syncConstraints)
-            .build()
-        WorkManager.getInstance(applicationContext).enqueue(request)
-    }
 }
--- app/src/main/java/com/hgabriel/gamemetascore/sync/SyncGameWorker.kt
+++ app/src/main/java/com/hgabriel/gamemetascore/sync/SyncGameWorker.kt
@@ -2,7 +2,12 @@ package com.hgabriel.gamemetascore.sync
 
 import android.content.Context
 import androidx.hilt.work.HiltWorker
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.hgabriel.gamemetascore.data.GameRepository
 import dagger.assisted.Assisted
@@ -20,5 +25,20 @@ class SyncGameWorker @AssistedInject constructor(
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         repository.sync()
         Result.success()
+    }
+
+    companion object {
+        fun sync(context: Context) {
+            val syncConstraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            val request = OneTimeWorkRequestBuilder<SyncGameWorker>()
+                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                .setConstraints(syncConstraints)
+                .build()
+
+            WorkManager.getInstance(context).enqueue(request)
+        }
     }
 }
